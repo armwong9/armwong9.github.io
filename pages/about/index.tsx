4f0cd9d1c8cb003de68a41db3a4112ec9f678383
armwong9 <60205398+armwong9@users.noreply.github.com>
2024-01-15 19:56:25 -0800

    order

--- pages/about/index.tsx
+++ pages/about/index.tsx
@@ -212,14 +212,14 @@ const About = ({ experiences, education }: AboutProps): JSX.Element => (
 export const getStaticProps: GetStaticProps = async () => {
   const experiences = await getPosts("experiences");
   experiences.sort((a, b) => {
-    // First, compare by date
-    const dateComparison = b.data.date.toString().localeCompare(a.data.date.toString());
-    if (dateComparison !== 0) {
-      return dateComparison;
+    // First, compare by orderId in reverse order
+    const orderComparison = b.data.orderId - a.data.orderId;
+    if (orderComparison !== 0) {
+      return orderComparison;
     }
   
-    // If dates are the same, then compare by orderId in reverse order
-    return b.data.orderId - a.data.orderId;
+    // If orderIds are the same, then compare by date
+    return b.data.date.toString().localeCompare(a.data.date.toString());
   });
 
   const education = await getPosts("education");
